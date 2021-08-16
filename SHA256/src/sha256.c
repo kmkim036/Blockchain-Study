e7ede1393035f28b86bf3617937d22aa2a85d873
kmkim036 <kmkim036@gmail.com>
2021-08-16 17:24:20 +0900

    Declare: a variable to use in loop

--- SHA256/src/sha256.c
+++ SHA256/src/sha256.c
@@ -3,16 +3,18 @@
 // pre-process of input message
 uint32_t* Make_W(char* buf)
 {
+    int i;
+    
     uint8_t msg[64] = { 0 };
-    for (int i = 0; i < strlen(buf); i++)
+    for (i = 0; i < strlen(buf); i++)
         msg[i] = buf[i];
-    msg[strlen(buf)] = 0x80;
-    msg[63] = strlen(buf) * 8;
+    msg[i] = 0x80;
+    msg[63] = i * 8;
 
     static uint32_t SHA256_W[64];
-    for (int i = 0; i < 16; i++)
+    for (i = 0; i < 16; i++)
         SHA256_W[i] = msg[i * 4] << 24 | msg[i * 4 + 1] << 16 | msg[i * 4 + 2] << 8 | msg[i * 4 + 3];
-    for (int i = 16; i < 64; i++)
+    for (i = 16; i < 64; i++)
         SHA256_W[i] = LOWERSIGMA1(SHA256_W[i - 2]) + SHA256_W[i - 7] + LOWERSIGMA0(SHA256_W[i - 15]) + SHA256_W[i - 16];
 
     return SHA256_W;
@@ -34,17 +36,19 @@ void Round_Func(uint32_t* H, uint32_t W, int i)
 
 uint32_t* Get_Hash(char* buf)
 {
+    int i;
+
     uint32_t* SHA256_W = Make_W(buf);
 
     static uint32_t SHA256_H[8] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
     uint32_t SHA256_H_TEMP[8];
-    for (int i = 0; i < 8; i++)
+    for (i = 0; i < 8; i++)
         SHA256_H_TEMP[i] = SHA256_H[i];
 
-    for (int i = 0; i < 64; i++)
+    for (i = 0; i < 64; i++)
         Round_Func(SHA256_H_TEMP, SHA256_W[i], i);
 
-    for (int i = 0; i < 8; i++)
+    for (i = 0; i < 8; i++)
         SHA256_H[i] += SHA256_H_TEMP[i];
 
     return SHA256_H;
