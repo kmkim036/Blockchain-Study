d5839a4872a0ea01db598f063c3c88800f5e3150
kmkim036 <kmkim036@gmail.com>
2021-08-17 12:58:05 +0900

    Add: main file for executing

---
+++ MerkleTree/src/main.c
@@ -0,0 +1,17 @@
+#include "merkletree.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        printf("Wrong Input Argument\nUsage: ./merkletree InputArgument0 InputArgument1 InputArgument2...\n\tExample:\t./merkletree TX0 TX1 TX2\n\tWrong Example:\t./merkletree\n");
+        return -1;
+    }
+    
+    printf("Initialize..\n");
+    Init_MerkleTree(argv + 1);
+
+    printf("\nPrint Hash Value by Post Order\n");
+    Print_MerkleHash_Postorder(root);
+
+    return 0;
+}
